6510efddebf12944022d1d34c7f8fc13f7cc4c25
CD-Mackay <connor.mackay@gmail.com>
2021-03-02 12:37:43 -0500

    refactor messagin component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Chat, Channel, ChannelHeader, Thread, Window } from 'stream-chat-react';
+import { Chat, Channel, ChannelHeader, ChannelList, Thread, Window } from 'stream-chat-react';
 import { MessageList, MessageInput } from 'stream-chat-react';
 import { StreamChat } from 'stream-chat';
+import { ChannelPreview } from './Components/ChannelPreview';
+import { MessageComponent } from './Components/MessageComponent';
 
 import 'stream-chat-react/dist/css/index.css';
 
 const chatClient = StreamChat.getInstance('dz5f4d5kzrue');
 const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoicGxhaW4tZGV3LTQiLCJleHAiOjE2MTQ3MjE2MDJ9.-IByyU7GN8jCMUISIbrIG0JxOL57P8hkbd_s65GRwP0';
+
+
+
 
 chatClient.connectUser(
   {
@@ -17,10 +22,10 @@ chatClient.connectUser(
   userToken,
 );
 
-const channel = chatClient.channel('messaging', 'godevs', {
-  image: 'https://cdn.chrisshort.net/testing-certificate-chains-in-go/GOPHER_MIC_DROP.png',
-  name: 'reactions to React',
-});
+// const channel = chatClient.channel('messaging', 'godevs', {
+//   image: 'https://cdn.chrisshort.net/testing-certificate-chains-in-go/GOPHER_MIC_DROP.png',
+//   name: 'reactions to React',
+// });
 
 const filters = { type: 'messaging', members: { $in: ['plain-dew-4'] } };
 const sort = { last_message_at: -1 };
@@ -28,7 +33,8 @@ const channels = chatClient.queryChannels(filters, sort);
 
 const App = () => (
   <Chat client={chatClient} theme='messaging light'>
-    <Channel filters={filters} sort={sort} channel={channel} >
+    <ChannelList filters={filters} sort={sort} Preview={ChannelPreview} />
+    <Channel Message={MessageComponent}>
       <Window>
         <ChannelHeader />
         <MessageList />
